213e5a61e9df41b6bb499c73656613df47f3cb5f
thedom85 <domenicozinzi@gmail.com>
2015-11-23 10:56:32 +0100

    Update and rename ColorRandom_HtmlDiv.js to ColorRandom_HtmlElements.js

--- ColorRandom_HtmlElements.js
+++ ColorRandom_HtmlElements.js
@@ -11,7 +11,7 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 $(document).ready(function()
 {
 	var colors = [ "blue","blue", "red", "yellow", "green", "grey" ];
-	$("*").find("div").each(function() {    
+	$("*").find("*").each(function() {    
 		$(this).css("border-color", colors[(Math.floor(Math.random() * (5 - 1 + 1)) + 1)] ).css("border-style","solid").css("border-width","medium");
 	});
 });
