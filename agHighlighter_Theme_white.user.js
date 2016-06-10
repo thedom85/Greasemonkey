91cb61ca4947e0a3a3c5d7aaed9ea29de64b045c
thedom85 <thedom85@users.noreply.github.com>
2016-06-10 10:33:39 +0200

    Create agHighlighter_Theme_white.user.js

---
+++ agHighlighter_Theme_white.user.js
@@ -0,0 +1,17 @@
+ ==UserScript==
+// @name        TagHighlighterThemeWhite
+// @namespace   https://github.com/thedom85/TagHighlighter
+// @description exec JS TagHighlighter on document loaded
+// @include     *
+// @require     https://raw.githubusercontent.com/thedom85/TagHighlighter/master/src/taghighlighter.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var fileref=document.createElement('script');
+fileref.setAttribute("type","text/javascript");
+fileref.setAttribute("src", "https://raw.githubusercontent.com/thedom85/TagHighlighter/master/src/taghighlighter.js");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  taghighlighter.Hlighter("*","white");
+});
