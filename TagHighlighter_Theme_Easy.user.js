b5772ae4c6985ccad18fe5172d51fe37be7eae7b
thedom85 <thedom85@users.noreply.github.com>
2016-06-11 11:41:32 +0200

    Create TagHighlighter_Theme_Easy.user.js

---
+++ TagHighlighter_Theme_Easy.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        TagHighlighterThemeEasy
+// @namespace   https://github.com/thedom85/TagHighlighter
+// @description exec JS TagHighlighter on document loaded
+// @include     *
+// @require     https://raw.githubusercontent.com/thedom85/TagHighlighter/master/src/taghighlighter.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+//Avoid conflict
+
+var fileref=document.createElement('script');
+fileref.setAttribute("type","text/javascript");
+fileref.setAttribute("src", "https://raw.githubusercontent.com/thedom85/TagHighlighter/master/src/taghighlighter.js");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  taghighlighter.Hlighter("*","easy");
+});
